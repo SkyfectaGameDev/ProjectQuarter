63ed5f0fbe4c8a9d736382682a10ab4c97c3ba1d
SkyfectaGameDev <skyfecta@gmail.com>
2023-01-13 17:13:36 +0000

    0.230113a - Project Setup & Game Basics
    System:
    - Removed the 3D and Spatial options in the editor
    - Locked the game to 60fps
    - Setup left, right & fire controls
    
    Ricochet:
    - Created main scene
    - Created block 1 sprite
    - Created temporary border objects
    - Created Player object
    - Created ball sprite
    - Added collision to borders
    - Created paddle script
    - Added basic paddle controls

---
+++ Games/Ricochet/Scripts/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+# ----- Variable Setup -----
+var velocity = Vector2.ZERO
+var acceleration = 300
+var friction = 125
+
+# ----- Moving The Paddle -----
+func _physics_process(delta):
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -acceleration
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = acceleration
+	
+	move_and_slide(velocity)
+	velocity = velocity.move_toward(Vector2.ZERO, friction)
