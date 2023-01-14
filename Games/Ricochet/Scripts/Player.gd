34646c5330d0abdd21d16815a2f9a068e99b6959
SkyfectaGameDev <skyfecta@gmail.com>
2023-01-14 21:20:14 +0000

    0.230114a -  Controller Support & Paddle Speed Adjustments
    System:
    - Added Controller support
    
    Rocket Ricochet:
    - Added the ability to slow the paddle down when “fire” is held down
    - Lowered the initial paddle speed from 300 to 250

--- Games/Ricochet/Scripts/Player.gd
+++ Games/Ricochet/Scripts/Player.gd
@@ -2,15 +2,21 @@ extends KinematicBody2D
 
 # ----- Variable Setup -----
 var velocity = Vector2.ZERO
-var acceleration = 300
+var acceleration = 250
+var speed_mult = 1
 var friction = 125
 
 # ----- Moving The Paddle -----
 func _physics_process(delta):
+	if (Input.is_action_pressed("fire")):
+		speed_mult = 0.5
+	else:
+		speed_mult = 1
+	
 	if (Input.is_action_pressed("move_left")):
-		velocity.x = -acceleration
+		velocity.x = -acceleration * speed_mult
 	elif (Input.is_action_pressed("move_right")):
-		velocity.x = acceleration
+		velocity.x = acceleration * speed_mult
 	
 	move_and_slide(velocity)
 	velocity = velocity.move_toward(Vector2.ZERO, friction)
